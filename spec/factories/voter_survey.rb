5d4b2d7e43b18ff33ce49dbcc6036d20d78cf15f
Hailey Somerville <hailey@hails.org>
2024-12-25 10:32:24 +1100

    spec/factories: ensure voter surveys have a voter
    rails belongs_to associations are now enforced in validation unless
    optional: true is set

--- spec/factories/voter_survey.rb
+++ spec/factories/voter_survey.rb
@@ -10,5 +10,6 @@ FactoryBot.define do
     has_received_other_grant { Faker::Boolean.boolean }
     how_many_fireflies { Faker::Number.between(from: 1, to: 20) }
     signed_agreement { Faker::Boolean.boolean }
+    voter
   end
 end
